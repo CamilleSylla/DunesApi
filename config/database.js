2d22ba63ce567ae31806421cd50b07caa4333ba6
Camille Sylla <camillesylla.web@gmail.com>
2022-01-08 06:44:41 +0100

    test for deployment

--- config/database.js
+++ config/database.js
@@ -1,24 +1,37 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "dunes"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "laGalere72"),
-        schema: env("DATABASE_SCHEMA", "public"),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'sammy'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-      options: {},
+      options: {
+        ssl: false,
+      },
     },
   },
 });
-
-//local db infos
-// path : C:\Program Files\PostgreSQL\14
-// path : for data C:\Program Files\PostgreSQL\14\data
-// mdp : laGalere72
-// port : 5432
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "localhost"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "dunes"),
+//         username: env("DATABASE_USERNAME", "postgres"),
+//         password: env("DATABASE_PASSWORD", "laGalere72"),
+//         schema: env("DATABASE_SCHEMA", "public"),
+//       },
+//       options: {},
+//     },
+//   },
+// });
