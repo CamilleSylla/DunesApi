a6a3db0cf13971ab51f235638901f4c79a82fd6c
Camille Sylla <camillesylla.web@gmail.com>
2022-01-09 19:32:31 +0100

    forced npm version

--- config/database.js
+++ config/database.js
@@ -24,14 +24,13 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "app-32495bb7-5ce0-4916-a95f-5d6da278e512-do-user-10569707-0.b.db.ondigitalocean.com"),
-        port: env.int("DATABASE_PORT", 25060),
-        database: env("DATABASE_NAME", "dunesdb"),
-        username: env("DATABASE_USERNAME", "dunesdb"),
-        password: env("DATABASE_PASSWORD", "sasQFUkVvBmsLEiL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dunes"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "laGalere72"),
         schema: env("DATABASE_SCHEMA", "public"),
-      },
-      options: {},
+      }
     },
   },
 });
