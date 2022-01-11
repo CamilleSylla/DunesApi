8533bca4618356124eb43d356ae82d2a1eb7786a
Camille Sylla <camillesylla.web@gmail.com>
2022-01-11 14:30:43 +0100

    config production changed

--- config/database.js
+++ config/database.js
@@ -1,37 +1,37 @@
-module.exports = () => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {},
-    },
-  },
-});
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
+// module.exports = () => ({
+//   defaultConnection: 'default',
 //   connections: {
 //     default: {
-//       connector: "bookshelf",
+//       connector: 'bookshelf',
 //       settings: {
-//         client: "postgres",
-//         host: env("DATABASE_HOST", "localhost"),
-//         port: env.int("DATABASE_PORT", 5432),
-//         database: env("DATABASE_NAME", "dunes"),
-//         username: env("DATABASE_USERNAME", "postgres"),
-//         password: env("DATABASE_PASSWORD", "laGalere72"),
-//         schema: env("DATABASE_SCHEMA", "public"),
-//       }
+//         client: 'postgres',
+//         host: process.env.DATABASE_HOST,
+//         port: process.env.DATABASE_PORT,
+//         database: process.env.DATABASE_NAME,
+//         username: process.env.DATABASE_USERNAME,
+//         password: process.env.DATABASE_PASSWORD,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       },
+//       options: {},
 //     },
 //   },
 // });
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dunes"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "laGalere72"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      }
+    },
+  },
+});
